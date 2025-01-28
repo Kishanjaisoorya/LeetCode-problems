a129d31f8b015986bf6d043d9e43194cba1e032a
Kishan Jai Soorya N <119406498+Kishanjaisoorya@users.noreply.github.com>
2025-01-28 18:54:14 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,7 +4,7 @@
     }
 }
 */
-class Solution {
+/*class Solution {
     public void sortColors(int[] nums) {
         int[] c = new int[3];
 
@@ -20,4 +20,31 @@ class Solution {
         }
 
     }
+}*/
+
+class Solution {
+    public void sortColors(int[] nums) {
+       int low = 0;
+       int mid =0;
+       int temp = 0;
+       int high = nums.length-1;
+       while(mid <= high){
+         if(nums[mid]==0){
+            temp = nums[mid];
+            nums[mid] = nums[low];
+            nums[low] = temp;
+            low++;
+            mid++;
+         }
+         else if(nums[mid]==2){
+            temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+            high--;
+         }
+         else{
+            mid++;
+         }
+    }
+ }
 }
